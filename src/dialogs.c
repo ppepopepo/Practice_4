4965568c6f0fe9830055a847f7e2c473b6682dae
Иванюк Агата <ppepopepo945@gmail.com>
2026-08-17 04:07:56 +0500

    Добавила диалоговые функции для ввода параметров

---
+++ src/dialogs.c
@@ -0,0 +1,15 @@
+/*
+dialogs.c - диалоговые окна для ввода параметров.
+
+Иванюк Агата
+МК-101
+*/
+
+#include <windows.h>
+#include "dialogs.h"
+
+int get_float_input(HWND parent, const char* title, float* value) {
+    char input[32] = { 0 };
+    // Упрощенная версия диалога
+    return 0;
+}
